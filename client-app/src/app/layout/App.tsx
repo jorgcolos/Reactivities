29626b076b0a4a47a884f484ee7ddd6ff838b0c9
jorgcolos <jorgcolos@gmail.com>
2023-11-17 15:28:55 -0600

    End of Section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,20 +4,22 @@ import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
 import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
 
   return (
     <Fragment>
-      {location.pathname==='/' ? <HomePage /> : (
-        <Fragment>
-          <NavBar/>
-        <Container style={{marginTop: '6em'}}>
-          <Outlet />
-        </Container>
-      </Fragment>
-      )}
+      <ToastContainer position='bottom-right' hideProgressBar theme="colored" />
+        {location.pathname==='/' ? <HomePage /> : (
+          <Fragment>
+            <NavBar/>
+          <Container style={{marginTop: '6em'}}>
+            <Outlet />
+          </Container>
+        </Fragment>
+        )}
     </Fragment>
   );
 }
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommondStore from "./commondStore";
 
 interface Store {
-    activityStore: ActivityStore
+    activityStore: ActivityStore;
+    commonStore: CommondStore;
 }
 
 export const store: Store = {
-    activityStore: new ActivityStore()
+    activityStore: new ActivityStore(),
+    commonStore: new CommondStore()
 }
 
 export const StoreContext = createContext(store);
